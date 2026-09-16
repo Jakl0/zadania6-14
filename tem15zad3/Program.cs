51289ad328f5775306fe51c1c5c99d314bd2f454
5M <5M@415-16>
2026-09-16 12:15:03 +0200

    dodanie zadan

---
+++ tem15zad3/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+namespace HelloWorld
+{
+   
+    internal class Program
+    {
+
+        static void WczytajOcenyUczniow(int[] uczniowie)
+
+        {
+
+            bool czySieDa = false;
+
+            int x;
+
+            for (int i = 0; i < uczniowie.Length; i++)
+            {
+                do
+                {
+                    Console.WriteLine($"Wpisz wynik z zakresu 0-100 dla ucznia nr {i+1}: ");
+                    string input = Console.ReadLine();
+
+                    czySieDa = (int.TryParse(input, out x)&& (x < 0 || x > 100));
+
+                } while (czySieDa);
+
+                uczniowie[i] = x;
+
+            }
+
+        }
+
+        /* 
+
+            Nazwa funkcji: ObliczSrednia 
+
+             Opis: Oblicza srednia arytmetyczna wynikow uzyskanych przez uczniow zapisanych w tablicy. 
+
+             Parametry: 
+
+                liczby – tablica liczb calkowitych, dla ktorych liczymy srednia 
+
+             Zwracana wartosc: brak; 
+
+             Autor: zdajacy nr 1 
+
+        */
+
+        static void ObliczSrednia(int[] uczniowie)
+        {
+
+            double suma = 0;
+
+            foreach (int x in uczniowie)
+            {
+
+                suma += x;
+
+            }
+
+            Console.WriteLine($"Średni wynik klasy wynosi {suma / uczniowie.Length}");
+
+        }
+        static void Wyswietl(int[] uczniowie)
+
+        {
+
+            int liczba =0;
+
+            foreach (int x in uczniowie)
+            {
+                if(x >= 50)
+                {
+
+                    liczba++;
+
+                }
+            }
+
+            Console.WriteLine($"Uczniow którzy zdali jest {liczba}");
+
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            int liczbauczniow;
+            bool czysieda;
+
+            do
+            {
+                Console.Write("Podaj liczbe uczniow: ");
+
+                string input = Console.ReadLine();
+
+                czysieda = (int.TryParse(input, out liczbauczniow) && liczbauczniow > 0);
+
+            } while (!czysieda);
+
+            int[] ocenyUczniow = new int[liczbauczniow];
+
+            WczytajOcenyUczniow(ocenyUczniow);
+
+            ObliczSrednia(ocenyUczniow);
+
+            Wyswietl(ocenyUczniow);
+
+        }
+    }
+}
--- tem9zad3/Program.cs
+++ tem9zad3/Program.cs
@@ -1,80 +1,99 @@
 ﻿using System;
 
-public class HelloWorld
-{
-    int IleParzystych(int[] tab)
-    {
-        int licz = 0;
-        foreach(int i in tab)
-        {
-            if (i % 2 == 0)
-            {
-                
-            }
-        }
-    }
-    public static void Main(string[] args)
-    {
-        Random random = new Random();
-        Console.WriteLine("Wpisz tu liczbe: ");
-        string input = Console.ReadLine();
-        int abc = int.Parse(input);
-        int[,] lis = new int[abc, 6];
-        int[] licznig = new int[49];
 
-        for (int i = 0; i < abc; i++)
+
+namespace Loteria
+
+{
+
+    class Program
+
+    {
+
+        
+
+        static void WypelnijZestaw(int[] liczby, Random generator)
+
         {
-            for (int j = 0; j < 6; j++)
+
+            for (int i = 0; i < liczby.Length; i++)
+
             {
-                lis[i, j] = random.Next(1, 49);
-                for (int k = 0; k < j; k++)
+
+                liczby[i] = generator.Next(1, 50); 
+
+            }
+
+        }
+
+ 
+
+        static void WyswietlZestaw(int numer, int[] liczby)
+
+        {
+
+            Console.Write($"Zestaw {numer}: ");
+
+            foreach (int liczba in liczby)
+
+            {
+
+                Console.Write(liczba + " ");
+
+            }
+            Console.Write($"- Liczby parzyste : {IleParzystych(liczby)}");
+            Console.WriteLine();
+
+        }
+
+        static int IleParzystych(int[] liczby)
+        {
+            int ilosc = 0;
+            foreach(int i in liczby)
+            {
+                if (i % 2 == 0)
                 {
-                    if (lis[i, k] == lis[i, j])
-                    {
-                        lis[i, j] = random.Next(1, 49);
-                        k--;
-                    }
+                    ilosc++;
                 }
             }
+            return ilosc;
         }
-        for (int i = 0; i < 49; i++)
-        {
-            licznig[i] = 0;
-        }
-        for (int i = 0; i < abc; i++)
-        {
-            for (int j = 0; j < 6; j++)
-            {
-                Console.Write($"{lis[i, j]} ");
-                licznig[lis[i, j] - 1] += 1;
-            }
-            Console.WriteLine();
-        }
-        int inde = 1;
-        int wartosc = 1;
-        List<int> odpowiedzi = new List<int>(1);
-        for (int i = 0; i < 49; i++)
-        {
-            if (wartosc < licznig[i])
-            {
-                inde = i;
-                wartosc = licznig[i];
-                odpowiedzi = new List<int>(inde);
-            }
-            else if (wartosc == licznig[i])
-            {
-                odpowiedzi.Add(i);
-            }
-        }
-        if (odpowiedzi.Count > 1)
-        {
-            Console.Write($"Liczby ");
-            foreach (int i in odpowiedzi)
-                Console.Write($"{i + 1} ");
-            Console.WriteLine($"byly wylosowane {wartosc} razy czyli najwiecej ze wszystkich");
-        }
-        else
-            Console.WriteLine($"Liczba {inde + 1} byla wylosowana {wartosc} razy czyli najwiecej ze wszystkich");
-    }
-}
 
+
+        static void Main(string[] args)
+
+        {
+
+            Random generator = new Random();
+
+
+
+            Console.Write("Ile zestawów wylosować? ");
+
+            int n = int.Parse(Console.ReadLine());
+
+
+
+            
+
+            for (int zestaw = 1; zestaw <= n; zestaw++)
+
+            {
+
+                int[] liczby = new int[6];   
+
+                WypelnijZestaw(liczby, generator); 
+
+                WyswietlZestaw(zestaw, liczby);  
+
+            }
+
+
+
+            Console.ReadKey();
+
+        }
+
+    }
+
+}
