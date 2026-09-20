61b2ae2cf8a6367f5a9d087305b903a3df4cb585
Jakl0 <ut_3999@zsp3zamosc.pl>
2026-09-20 21:07:33 +0200

    dodanie zadan

---
+++ tem19zad3/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace temat19
+{
+
+    internal class Program
+    {
+
+        static void WczytajLiczby(int[] liczby)
+        {
+
+            for(int i = 0; i < liczby.Length; i++)
+            {
+                bool czysieda = false;
+                Console.Write($"{i+1}.");
+                do
+                {
+                    Console.Write("Wpisz Liczbę całkowitą: ");
+
+                    string input = Console.ReadLine();
+
+                    czysieda = int.TryParse(input, out liczby[i]);
+
+                } while (!czysieda);
+    
+            }
+
+        }
+
+        /********************************************** 
+
+        * Nazwa funkcji: ObliczSume
+
+        * Opis działania:  dodaje wszystkie liczby w tablicy
+
+        * Parametry: liczby - tablica liczb wpisanych przez użytkownika
+
+        * Zwracana wartość: suma wszystkich liczb 
+
+        * Autor: zdający nr 7
+
+        **********************************************/
+        static int ObliczSume(int[] liczby)
+        {
+            int suma =0;
+
+            foreach(int liczba in liczby)
+            {
+
+                suma += liczba;
+
+            }
+
+            return suma;
+
+        }
+
+        static int PoliczDodatnie(int[] liczby)
+        {
+            int dodatnie = 0;
+
+            foreach (int liczba in liczby)
+            {
+
+                if (liczba > 0)
+                {
+
+                    dodatnie++;
+
+                }
+
+            }
+
+            return dodatnie;
+
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            int[] liczby = new int[5];
+            
+            WczytajLiczby(liczby);
+
+            Console.WriteLine($"Suma twoich liczb wynosi: {ObliczSume(liczby)}");
+
+            Console.WriteLine($"Liczba wartości dodatnich : {PoliczDodatnie(liczby)}");
+            
+
+        }
+
+    }
+
+}
---
+++ tem20zad3/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+
+namespace temat19
+{
+
+    internal class Program
+    {
+
+        static int WczytajLiczbe()
+        {
+
+            bool czyliczba , czysieda = false;
+            int liczbauczniow;
+            do
+            {
+
+                Console.Write("Podaj liczbe uczniow 1-40: ");
+                
+                string input = Console.ReadLine();
+
+                czyliczba = int.TryParse(input, out liczbauczniow);
+
+                czysieda = czyliczba && (liczbauczniow>0 && liczbauczniow < 40);
+
+            } while (!czysieda);
+
+            return liczbauczniow;
+        }
+
+        /********************************************** 
+
+        * Nazwa funkcji: WczytajOceny
+
+        * Opis działania:  wczytuje odpowiednie liczby od użytkownika do tablicy
+
+        * Parametry: oceny  - tablica liczb (ocen uczniow)
+
+        * Zwracana wartość: brak
+
+        * Autor: zdający nr 25
+
+        **********************************************/
+        static void WczytajOceny(int[] oceny)
+        {
+            
+            for(int i = 0; i < oceny.Length; i++)
+            {
+
+                bool czyliczba, czysieda = false;
+                Console.Write($"{i + 1}. ");
+                do
+                {
+
+                    Console.Write("Podaj ocenę ucznia 1-6: ");
+
+                    string input = Console.ReadLine();
+
+                    czyliczba = int.TryParse(input, out oceny[i]);
+
+                    czysieda = czyliczba && (oceny[i] > 0 && oceny[i] <= 6);
+
+                } while (!czysieda);
+
+            }
+
+        }
+
+        static void Wyswietl(int[] oceny)
+        {
+
+            int suma = 0;
+            int pomocnicza =1;
+            
+            foreach(int ocena in oceny)
+            {
+                
+                if(ocena > pomocnicza)
+                {
+
+                    pomocnicza = ocena;
+
+                }
+
+            }
+
+            Console.WriteLine("Najwyższa ocena to " + pomocnicza);
+
+            foreach (int ocena in oceny)
+            {
+
+                if (ocena < pomocnicza)
+                {
+
+                    pomocnicza = ocena;
+
+                }
+
+            }
+
+            Console.WriteLine("Najniższa ocena to " + pomocnicza);
+
+            foreach (int ocena in oceny)
+            {
+
+                suma += ocena;
+
+            }
+            suma = suma / oceny.Length;
+
+            Console.WriteLine($"Średnia ocen wynosi {suma:F2}");
+
+
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            int liczbauczniow=0;
+
+            liczbauczniow = WczytajLiczbe();
+
+            int[] oceny = new int[liczbauczniow];
+
+            WczytajOceny(oceny);
+
+            Wyswietl(oceny);
+
+        }
+
+    }
+
+}
